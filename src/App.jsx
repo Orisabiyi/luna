55dac021a8355d41b984371c794f3bfcc26965bc
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-29 13:04:43 +0100

    chore: Update dependencies and add react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
