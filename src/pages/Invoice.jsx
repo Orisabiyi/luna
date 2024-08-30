8814ef520e3d537b04e133c05cc49d1dbd5d0f3a
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-30 13:17:30 +0100

    feat: Update Invoice page to display user address

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -6,6 +6,9 @@ function Invoice() {
   const [userAddress] = useState(function () {
     return JSON.parse(sessionStorage.getItem("address"));
   });
+
+  const { userProfile } = UserContext();
+
   const [issuedDate, setIssuedDate] = useState("");
 
   useEffect(function () {
@@ -17,7 +20,7 @@ function Invoice() {
       <figure className="bg-white px-[3rem] py-[1.5rem] w-[50%]">
         <div className="text-[2rem]">
           <h1>{`Invoice LN-${new Date().toISOString().slice(-4)}`}</h1>
-          <h3>{`Issued Date`}</h3>
+          <h3>{`Issued Date: ${issuedDate}`}</h3>
         </div>
 
         <div className="text-[1.8rem]">
@@ -26,8 +29,12 @@ function Invoice() {
           </p>
 
           <p>
-            <span>From:</span>
-            {/* <span>{userAddress.profile.stxAddress.mainnet}</span> */}
+            <span>From: </span>
+            <span>
+              {userProfile.length > 0
+                ? userProfile.profile.stxAddress.mainnet
+                : userAddress.profile.stxAddress.mainnet}
+            </span>
           </p>
         </div>
       </figure>
