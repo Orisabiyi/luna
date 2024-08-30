6404404856c107054f4d7974e1fee28d9696ff1b
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-30 07:13:44 +0100

    feat: Add homepage route and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
