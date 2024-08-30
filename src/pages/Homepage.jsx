1ce2f88144c4cb056d7c71e2c1e74fab001b9bbf
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-30 07:29:43 +0100

    chore: Update homepage with wallet connection button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,13 @@
 function Homepage() {
-  return <div></div>;
+  return (
+    <header className="bg-black min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-white text-[10rem]">Luna</h1>
+
+      <button className="px-[3rem] py-[1rem] bg-gray-300 text-[2rem] rounded-[50rem] hover:translate-y-[-1rem] transition duration-700 ease-in-out delay-300">
+        Connect Wallet
+      </button>
+    </header>
+  );
 }
 
 export default Homepage;
