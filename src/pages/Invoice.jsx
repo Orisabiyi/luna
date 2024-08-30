82b334b8d27ea147ed56fa9dd9153ab4d49bb252
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-30 12:49:18 +0100

    chore: Add react-contenteditable dependency and update prop-types version

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,5 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+
 function Invoice() {
-  return <section>Hello Invoice</section>;
+  const [issuedDate, setIssuedDate] = useState("");
+  const { userProfile } = UserContext();
+
+  useEffect(
+    function () {
+      setIssuedDate(new Date().toLocaleDateString());
+      console.log(userProfile);
+    },
+    [userProfile]
+  );
+
+  return (
+    <section className="min-h-screen bg-black flex flex-col items-center justify-center">
+      <figure className="bg-white px-[3rem] py-[1.5rem] w-[50%]">
+        <div className="text-[2rem]">
+          <h1>{`Invoice LN-${new Date().toISOString().slice(-4)}`}</h1>
+          <h3>{`Issued Date`}</h3>
+        </div>
+
+        <div className="text-[1.8rem]">
+          <p>
+            <span>To:</span>
+          </p>
+
+          <p>
+            <span>From:</span>
+            <span>{userProfile.profile.stxAddress.mainnet}</span>
+          </p>
+        </div>
+      </figure>
+    </section>
+  );
 }
 
 export default Invoice;
