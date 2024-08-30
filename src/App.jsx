b9d11c06723d20fa078930d3bcd23c7c3c53e3f5
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-30 07:58:53 +0100

    feat: Add invoice page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Invoice from "./pages/Invoice";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/inovice" element={<Invoice />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Invoice.jsx
@@ -0,0 +1,5 @@
+function Invoice() {
+  return <section>Hello Invoice</section>;
+}
+
+export default Invoice;
