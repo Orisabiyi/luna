a2356a2e1aad6c40a97c98066a57960d444f5624
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 18:58:12 +0100

    feat: Refactor InvoiceButton to use navigation for creating new invoices

--- src/components/InvoiceButton.jsx
+++ src/components/InvoiceButton.jsx
@@ -1,16 +1,13 @@
-import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-export default function InvoiceButton({ handler }) {
+export default function InvoiceButton() {
+  const navigate = useNavigate("");
   return (
     <button
       className="border-2 rounded-xl px-8 py-4 cursor-pointer"
-      onClick={handler}
+      onClick={() => navigate("/invoice")}
     >
       + New Invoice
     </button>
   );
 }
-
-InvoiceButton.propTypes = {
-  handler: PropTypes.func,
-};
