36fe6ba6d121fe486b5f435bb1f18d8571336eff
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 13:02:04 +0100

    feat: Update AllInvoice page to include invoice icon and styling

--- src/pages/AllInvoice.jsx
+++ src/pages/AllInvoice.jsx
@@ -1,3 +1,11 @@
+import { FaFileInvoiceDollar } from "react-icons/fa";
+
 export default function AllInvoice() {
-  return "Hello World";
+  return (
+    <main className="h-screen bg-black">
+      <section className="flex items-center justify-center">
+        <FaFileInvoiceDollar color="white" size={50} />
+      </section>
+    </main>
+  );
 }
