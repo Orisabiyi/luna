02b54511b1a6aeee8571e7019b52744aaf2bcce9
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 19:47:26 +0100

    style: Adjust Navbar padding for improved layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import InvoiceButton from "./InvoiceButton";
 
 export default function Navbar() {
   return (
-    <nav className="text-white flex items-center justify-between p-20 text-3xl">
+    <nav className="text-white flex items-center justify-between py-12 px-32 text-3xl">
       <h1 className="text-5xl font-semibold">Luna</h1>
       <InvoiceButton />
     </nav>
