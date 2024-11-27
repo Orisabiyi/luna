692a0cd01cbef2b71794131c0c16f633ef848150
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 18:59:57 +0100

    fix: Remove empty string argument from useNavigate in InvoiceButton component

--- src/components/InvoiceButton.jsx
+++ src/components/InvoiceButton.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function InvoiceButton() {
   const navigate = useNavigate("");
+
   return (
     <button
       className="border-2 rounded-xl px-8 py-4 cursor-pointer"
