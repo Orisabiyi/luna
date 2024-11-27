87b44a0a46556ba6ca054b3fe41169dbf1b2f310
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 18:04:37 +0100

    feat: Replace static New Invoice button with InvoiceButton component in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
+import InvoiceButton from "./InvoiceButton";
+
 export default function Navbar() {
   return (
     <nav className="text-white flex items-center justify-between p-20 text-3xl">
       <h1 className="text-5xl font-semibold">Luna</h1>
-      <button className="border-2 rounded-xl px-8 py-4 cursor-pointer">
-        + New Invoice
-      </button>
+      <InvoiceButton />
     </nav>
   );
 }
