102e720d97923ad373a5dcc1cb079975f7ca0d4d
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 17:33:15 +0100

    feat: Improve Navbar styling with flex layout and enhanced button design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
-    <nav className="text-white">
-      <h1>Luna</h1>
-      <button>+ New Invoice</button>
+    <nav className="text-white flex items-center justify-between p-20 text-3xl">
+      <h1 className="text-5xl font-semibold">Luna</h1>
+      <button className="border-2 rounded-xl px-8 py-4">+ New Invoice</button>
     </nav>
   );
 }
