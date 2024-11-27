afb726cc148f6f6f22e4223eba2c04c80d53bb1a
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-27 17:40:54 +0100

    feat: Update Navbar and AllInvoice layout with new button styling and message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,9 @@ export default function Navbar() {
   return (
     <nav className="text-white flex items-center justify-between p-20 text-3xl">
       <h1 className="text-5xl font-semibold">Luna</h1>
-      <button className="border-2 rounded-xl px-8 py-4">+ New Invoice</button>
+      <button className="border-2 rounded-xl px-8 py-4 cursor-pointer">
+        + New Invoice
+      </button>
     </nav>
   );
 }
--- src/pages/AllInvoice.jsx
+++ src/pages/AllInvoice.jsx
@@ -3,11 +3,15 @@ import Navbar from "../components/Navbar";
 
 export default function AllInvoice() {
   return (
-    <main className="h-screen bg-black">
+    <main className="h-screen bg-black flex flex-col">
       <Navbar />
 
-      <section className="flex items-center justify-center h-full">
+      <section className="flex flex-col items-center justify-center gap-8 flex-1 text-white text-3xl">
         <FaFileInvoiceDollar color="white" size={100} />
+        <p>You currently have no invoice</p>
+        <button className="border-2 rounded-xl px-8 py-4 cursor-pointer">
+          + New Invoice
+        </button>
       </section>
     </main>
   );
