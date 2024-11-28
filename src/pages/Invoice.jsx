67df0d276c20c65d3c2ec949d6577eabaf86d021
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-28 22:00:54 +0100

    feat: Implement dynamic input fields for products/services in Invoice component

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 export default function Invoice() {
+  const [inputs, setInputs] = useState([
+    [
+      { id: 1, type: "text", value: "" },
+      { id: 1, type: "number", value: "" },
+    ],
+    [
+      { id: 2, type: "text", value: "" },
+      { id: 2, type: "number", value: "" },
+    ],
+  ]);
+
+  const handleAddInputs = function () {
+    console.log(inputs.length);
+
+    setInputs((prevInput) => [
+      ...prevInput,
+      [
+        { id: inputs.length + 1, type: "text", value: "" },
+        { id: inputs.length + 1, type: "number", value: "" },
+      ],
+    ]);
+  };
+
   return (
-    <main className="bg-black h-screen flex flex-col">
+    <main className="bg-black min-h-screen flex flex-col">
       <Navbar />
 
       <section className="flex items-center flex-1 p-32">
         <form className="w-3/5 h-full flex flex-col gap-6 text-4xl">
           <label htmlFor="invoice-title" className="text-white">
-            Title
+            Title*
           </label>
           <input
             type="text"
@@ -18,13 +42,36 @@ export default function Invoice() {
           />
 
           <label htmlFor="recipient" className="text-white">
-            Recipient Wallet Address
+            Recipient Wallet Address*
           </label>
           <input
             type="text"
             id="recipient"
-            className="w-full p-5 outline-none rounded-lg text-3xl"
+            className="w-full p-5 outline-none rounded-lg text-3xl mb-8"
+            required
           />
+
+          <label htmlFor="recipient" className="text-white">
+            Product/Service Rendered*
+          </label>
+          <ul className="flex flex-col justify-between gap-8 w-full">
+            {inputs.map((input, i) => (
+              <li key={i} className="flex items-center justify-start gap-8">
+                {input.map((item, j) => (
+                  <input
+                    type={item.type}
+                    value={item.value}
+                    key={`${i}-${j}`}
+                    className="w-1/2 p-5 outline-none rounded-lg text-3xl"
+                  />
+                ))}
+              </li>
+            ))}
+          </ul>
+
+          <button className="text-white" onClick={handleAddInputs}>
+            Add
+          </button>
         </form>
 
         <figure></figure>
