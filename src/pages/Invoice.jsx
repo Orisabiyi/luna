164246d0875c400cfae2d200e0e17442c541fafd
Orisabiyi <orisabiyidavid@gmail.com>
2024-11-29 07:41:30 +0100

    feat: Enhance Invoice component with form input styling and prevent default on button click

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -13,7 +13,8 @@ export default function Invoice() {
     ],
   ]);
 
-  const handleAddInputs = function () {
+  const handleAddInputs = function (e) {
+    e.preventDefault();
     console.log(inputs.length);
 
     setInputs((prevInput) => [
@@ -31,27 +32,27 @@ export default function Invoice() {
 
       <section className="flex items-center flex-1 p-32">
         <form className="w-3/5 h-full flex flex-col gap-6 text-4xl">
-          <label htmlFor="invoice-title" className="text-white">
+          <label htmlFor="invoice-title" className="text-gray-200">
             Title*
           </label>
           <input
             type="text"
-            className="w-full p-5 outline-none rounded-lg text-3xl mb-8"
+            className="w-full p-5 outline-none rounded-lg text-3xl mb-8 bg-gray-100"
             id="invoice-title"
             required
           />
 
-          <label htmlFor="recipient" className="text-white">
+          <label htmlFor="recipient" className="text-gray-200">
             Recipient Wallet Address*
           </label>
           <input
             type="text"
             id="recipient"
-            className="w-full p-5 outline-none rounded-lg text-3xl mb-8"
+            className="w-full p-5 outline-none rounded-lg text-3xl mb-8 bg-gray-100"
             required
           />
 
-          <label htmlFor="recipient" className="text-white">
+          <label htmlFor="recipient" className="text-gray-200">
             Product/Service Rendered*
           </label>
           <ul className="flex flex-col justify-between gap-8 w-full">
@@ -62,15 +63,18 @@ export default function Invoice() {
                     type={item.type}
                     value={item.value}
                     key={`${i}-${j}`}
-                    className="w-1/2 p-5 outline-none rounded-lg text-3xl"
+                    className="w-1/2 p-5 outline-none rounded-lg text-3xl bg-gray-100"
                   />
                 ))}
               </li>
             ))}
           </ul>
 
-          <button className="text-white" onClick={handleAddInputs}>
-            Add
+          <button
+            className="bg-gray-200 py-4 rounded-lg mt-2 cursor-pointer"
+            onClick={handleAddInputs}
+          >
+            +
           </button>
         </form>
 
